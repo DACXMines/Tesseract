c3117d9eb15f0588a2925755d72da2004a681812
Martin Bouthiaux <martin2366@hotmail.fr>
2022-02-03 15:22:42 +0100

    Structure

---
+++ attack.py
@@ -0,0 +1,9 @@
+import torch 
+import torch.nn as nn 
+import numpy as np 
+
+
+#SOTA untargeter model poisoning attacj 
+def directed_deviation_attach(): 
+    return None 
+
---
+++ defense.py
@@ -0,0 +1,22 @@
+import torch 
+import torch.nn as nn 
+import numpy as np 
+
+
+#State of the art defense systems : 
+class Krum():
+    
+
+class Bulyan() : 
+
+
+class Trimmed_mean(): 
+
+
+class foolsgold() : 
+
+
+class faba():  
+
+
+#Tesseract : 
