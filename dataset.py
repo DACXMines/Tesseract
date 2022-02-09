76a5def3476a0761f85df0f6bda749195aa94fdb
unknown <pierrebossuet0@gmail.com>
2022-02-10 00:12:04 +0100

    normalized the datasets

--- dataset.py
+++ dataset.py
@@ -1,11 +1,12 @@
 import torch
 from torch.utils.data import Dataset
 from torchvision import datasets
-from torchvision.transforms import ToTensor
+import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
 import yaml
 import tensorflow as tf
 
+'''
 with open(r'cofig.yaml') as file:
     # The FullLoader parameter handles the conversion from YAML
     # scalar values to Python the dictionary format
@@ -13,70 +14,77 @@ with open(r'cofig.yaml') as file:
 
 dataset = params['dataset']
 batch_size = params['batch_size']
+'''
+
 
 class DataPipeline :
     def __init__(self, dataset_name : str, batch_size : int) -> None:
         self.batch_size = batch_size
         self.dataset_name = dataset_name
 
-        if dataset_name == 'FashionMNIST' :
+        if self.dataset_name == 'FashionMNIST' :
             self.training_data = datasets.FashionMNIST(
                 root="data",
                 train=True,
                 download=True,
-                transform=ToTensor()
+                transform=transforms.Compose([
+                       transforms.ToTensor(),
+                       transforms.Normalize((0.5,), (0.5,))
+                   ])
             )
 
             self.test_data = datasets.FashionMNIST(
                 root="data",
                 train=False,
                 download=True,
-                transform=ToTensor()
+                transform=transforms.Compose([
+                       transforms.ToTensor(),
+                       transforms.Normalize((0.5,), (0.5,))
+                   ])
             )
-        elif self.model_name == 'MNIST':
+        elif self.dataset_name == 'MNIST':
             self.training_data = datasets.MNIST(
                 root="data",
                 train=True,
                 download=True,
-                transform=ToTensor()
+                transform=transforms.Compose([
+                       transforms.ToTensor(),
+                       transforms.Normalize((0.1307,), (0.3081,))
+                   ])
             )
 
             self.test_data = datasets.MNIST(
                 root="data",
                 train=False,
                 download=True,
-                transform=ToTensor()
+                transform=transforms.Compose([
+                       transforms.ToTensor(),
+                       transforms.Normalize((0.1307,), (0.3081,))
+                   ])
             )
-        elif self.model_name == 'CIFAR10':
+        elif self.dataset_name == 'CIFAR10':
             self.training_data = datasets.CIFAR10(
                 root="data",
                 train=True,
                 download=True,
-                transform=ToTensor()
+                transform=transforms.Compose([
+                    transforms.ToTensor(),
+                    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+                    ])
             )
 
             self.test_data = datasets.CIFAR10(
                 root="data",
                 train=False,
                 download=True,
-                transform=ToTensor()
+                transform=transforms.Compose([
+                    transforms.ToTensor(),
+                    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+                    ])
             )
-        elif self.model_name == 'CIFAR10':
-            self.training_data = datasets.CIFAR10(
-                root="data",
-                train=True,
-                download=True,
-                transform=ToTensor()
-            )
-
-            self.test_data = datasets.CIFAR10(
-                root="data",
-                train=False,
-                download=True,
-                transform=ToTensor()
-            )
+        
         else :
-            raise NotImplementedError(f'{self.model_name} is not implemented yet.')
+            raise NotImplementedError(f'{self.dataset_name} dataset not available yet.')
 
 
 
