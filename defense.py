abc0f723535095ba8c518f9a297ff3a78b9cf63d
Martin Bouthiaux <martin2366@hotmail.fr>
2022-02-09 16:09:30 +0100

    fedsgd

--- defense.py
+++ defense.py
@@ -4,6 +4,17 @@ import numpy as np
 
 
 #State of the art defense systems : 
+class DefenseSystem() : 
+    
+    def __init__(self, grads) : 
+        self.grads = grads
+        self.n_clients = len(grads)
+
+    def fed_sgd(self):
+        n = self.n_clients 
+        weighted_mean = np.mean(self.grads)
+        
+
 class Krum():
     
 
